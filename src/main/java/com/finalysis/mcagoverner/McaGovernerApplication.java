75679b949d2f2d1345a513bf1a6728dd10d43a24
Vinoth G <vin10catchme@gmail.com>
2019-03-21 13:01:50 +0530

    Iniital commit for MCA Governer

---
+++ src/main/java/com/finalysis/mcagoverner/McaGovernerApplication.java
@@ -0,0 +1,13 @@
+package com.finalysis.mcagoverner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McaGovernerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McaGovernerApplication.class, args);
+	}
+
+}
